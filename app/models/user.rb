139a0f87fcfb131b9704cb5ffc29d10fdb07f462
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-10-28 11:15:02 +0000

    [Add]relationshipのcontroller/routing

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,10 +7,16 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed 
+  has_many :follower_user, through: :followed, source: :follower
+
   has_one_attached :profile_image
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, length: { maximum: 50 }
+  
 
   def get_profile_image(width,hight)
     unless profile_image.attached?
@@ -18,5 +24,17 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
     profile_image.variant(resize_to_limit: [100, 100]).processed
+  end
+  
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+
+  def following?(user)
+    following_user.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/follows'
+  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
@@ -8,9 +10,13 @@ Rails.application.routes.draw do
 
   resources :books, only: [:index,:show,:edit,:create,:destroy,:update] do
     resources :book_comments, only: [:create, :destroy]
-    resource :favorites, only: [:create,:destroy] 
+    resource :favorites, only: [:create,:destroy]
   end
-  resources :users, only: [:index,:show,:edit,:update]
-  
+  resources :users, only: [:index,:show,:edit,:update] do
+    member do
+    get :follows, :followers
+    end
+    resource :relationships, only: [:create,:destroy]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
