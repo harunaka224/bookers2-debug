25d952062b87a95ffd01c6cd0793d14db9428772
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-10-30 12:28:58 +0000

    [Start]応用５

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'relationships/followings'
-  get 'relationships/followers'
-  get 'relationships/follows'
-  get 'relationships/followers'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   root :to =>"homes#top"
   get "home/about"=>"homes#about"
+  get "relationships/followings"
+  get "relationships/followers"
+  get "search" => "searches#search"
 
   devise_for :users
 
