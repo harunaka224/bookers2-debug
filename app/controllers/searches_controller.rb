6454fc74f36253af5d506b77bf3ca419c5df753b
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-10-31 02:39:27 +0000

    [Add]検索機能

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -6,7 +6,7 @@ class SearchesController < ApplicationController
     if @range == "User"
       @users = User.looks(params[:search], params[:word])
     else
-      @books = Books.looks(params[:search], params[:word])
+      @books = Book.looks(params[:search], params[:word])
     end
   end
 end
