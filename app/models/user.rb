54116da3c54c9dd183478747527d4ccd472142eb
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-05 06:14:43 +0000

    [Fix]いいね非同期通信化

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :books, dependent: :destroy
-  has_many :favorites, dependent: :destrovy
+  has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  has_many :favorited_books, through: :favorites, source: :book
-  
+ 
   has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy 
   has_many :followers, through: :reverse_of_relationships, source: :follower
   
