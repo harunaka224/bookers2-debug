2c42c92ed651c36d80d5d15da68e78039b9faafa
harunaka224 <114799587+harunaka224@users.noreply.github.com>
2022-11-06 08:13:15 +0000

    [Add]いいねランキング

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,15 +2,13 @@ class Book < ApplicationRecord
   belongs_to :user
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
-  
-  
   validates :title,presence:true
   validates :body,presence:true,length:{maximum:200}
-  
+
   def favorited_by?(user)
-    favorites.exists?(user_id: user.id)
+    favorites.where(user_id: user.id).exists?
   end
-  
+
   def self.search_for(content, method)
     if method == "perfect"
       Book.where(title: content)
@@ -22,4 +20,5 @@ class Book < ApplicationRecord
       Book.where('title LIKE ?', '%' + content + '%')
     end
   end
+
 end
